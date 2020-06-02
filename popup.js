b40cfa4331343ce9a0947075000d13ae1eb58ca4
Jason Lee <j.lee10@outlook.com>
2020-06-01 23:01:01 -0700

    filters are now saved

---
+++ popup.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('submit_button');
+
+    link.addEventListener('click', function() {
+        submit();
+    });
+
+    // data is an obj with key value pairings
+    chrome.storage.sync.get({list:[]}, function (data) {
+        var text = document.getElementById("filter_box");
+        var a = [];
+        console.log(data.list);
+
+        for(let i = 0; i < data.list.length; i++) {
+            a.push(data.list[i]);
+        }
+            
+        text.innerText = a.join(',');
+    });
+});
+
+function submit() {
+    var text = document.getElementById("filter_box").value;
+    var words = text.split(",");
+
+    chrome.storage.sync.set({list:words}, function() {
+        console.log("filtered words are " + words);
+    });
+
+
+    
+}
