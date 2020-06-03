0af0831d67eff03b8b9741f2cfdf39d05acce70f
Jason Lee <j.lee10@outlook.com>
2020-06-02 23:54:27 -0700

    finds comments

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-console.log("Youtube")
+
+function check_page(){
+    var ready_state = document.readyState;
+    console.log("DOCUMENT READY STATE " + ready_state);
+
+    if (ready_state == "complete") {
+        var comment = document.querySelectorAll("#content-text");
+    
+        comment.forEach(function(text) {
+            console.log("COMMENT: " + text.innerText);
+        });
+    }
+
+}
+
+setInterval(check_page, 1000);
+
