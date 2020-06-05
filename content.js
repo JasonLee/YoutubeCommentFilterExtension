3169d05620fbae18d1b28b48078a5074394dea31
Jason Lee <j.lee10@outlook.com>
2020-06-04 23:07:17 -0700

    filtered comments show a spoiler

--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 
 function check_page(){
     var ready_state = document.readyState;
-    console.log("DOCUMENT READY STATE " + ready_state);
 
     if (ready_state == "complete") {
-        var comments = document.querySelectorAll("#content-text");
+        var comments = document.querySelectorAll("#content-text, #content-text > span");
     
         // Key: Default
         chrome.storage.sync.get({list: []}, function (data) {
             comments.forEach(function(comment) {
                 for(let i = 0; i < data.list.length; i++) {
-                    if(comment.innerText.includes(data.list[i])) {
-                        comment.innerText = "SPAM";
+                    if(!isNotSpoilerAlready(comment) && checkWord(data.list[i], comment.innerText)) {
+                        comment.innerHTML = "<details><summary>SPAM</summary>" + comment.innerHTML + "</details>";
+                        // comment.innerText = "SPAM";
                         break;
                     }
                 }
@@ -20,6 +20,18 @@ function check_page(){
 
     }
 
+}
+
+function checkWord(word, str) {
+    const allowedSeparator = '\\\s,.;\n\r"\'|:-';
+  
+    const regex = new RegExp(`(^.*[${allowedSeparator}]${word}$)|(^${word}[${allowedSeparator}].*)|(^${word}$)|(^.*[${allowedSeparator}]${word}[${allowedSeparator}].*$)`, 'i',);
+    
+    return regex.test(str);
+}
+
+function isNotSpoilerAlready(comment) {
+    return comment.innerText.startsWith("<details>")
 }
 
 setInterval(check_page, 1000);
--- popup.js
+++ popup.js
@@ -21,7 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function submit() {
     var text = document.getElementById("filter_box").value;
+    // Sanitise inputs
     var words = text.split(",");
+
+    words.forEach(element => {
+        element = element.trim();
+    });
 
     chrome.storage.sync.set({list:words}, function() {
         console.log("filtered words are " + words);
