a524d153df7ce198818528394b3693867f60a617
Jason Lee <j.lee10@outlook.com>
2020-06-09 22:40:54 -0700

    finished buttons and fixed filters

--- content.js
+++ content.js
@@ -1,24 +1,21 @@
-
-function check_page(){
+function checkPage() {
     var ready_state = document.readyState;
 
+    // comments load at this state and when users scroll down further
     if (ready_state == "complete") {
         var comments = document.querySelectorAll("#content-text, #content-text > span");
-       
+
         // Key: Default
-        chrome.storage.sync.get({list: [], case_sens:false, exact:false}, function (data) {
-            if(data.list.length <= 1 && !data.list[0].trim()){
+        chrome.storage.sync.get({ list: [], case_sens: false, exact: false }, function(data) {
+            // Checks if list is empty or contains 1 element of ""
+            if (data.list.length <= 1 && !data.list[0].trim()) {
                 return;
             }
 
             comments.forEach(function(comment) {
-                for(let i = 0; i < data.list.length; i++) {
-                    if(!isSpoilerAlready(comment) && checkWord(data.list[i], comment.innerText, data)) {
-                        console.log("comment.parentElement.nodeName = " + comment.parentElement.nodeName);
-                        console.log("BEFORE comment.innerHTML = " + comment.innerHTML);
+                for (let i = 0; i < data.list.length; i++) {
+                    if (!isSpoilerAlready(comment) && checkWord(data.list[i], comment.innerText, data)) {
                         comment.innerHTML = "<details><summary>SPAM</summary>" + comment.innerHTML + "</details>";
-                        console.log("AFTER comment.innerHTML = " + comment.innerHTML);
-                        console.log("Is Spoiler? " + isSpoilerAlready(comment));
                         break;
                     }
                 }
@@ -32,21 +29,31 @@ function check_page(){
 function checkWord(word, str, data) {
     const allowedSeparator = '\\\s,.;\n\r"\'|:-?';
 
-    if(data.exact && data.case_sens) {
-        const regex = new RegExp(`(^.*[${allowedSeparator}]${word}$)|(^${word}[${allowedSeparator}].*)|(^${word}$)|(^.*[${allowedSeparator}]${word}[${allowedSeparator}].*$)`);
+    if (data.exact && data.case_sens) {
+        const regex = new RegExp(`.*\\b${word}\\b.*`);
         return regex.test(str);
-    }else if(data.exact){
-        const regex = new RegExp(`(^.*[${allowedSeparator}]${word}$)|(^${word}[${allowedSeparator}].*)|(^${word}$)|(^.*[${allowedSeparator}]${word}[${allowedSeparator}].*$)`, 'i');
+    } else if (data.exact) {
+        const regex = new RegExp(`.*\\b${word}\\b.*`, 'i');
         return regex.test(str);
-    }else{
+    } else if (data.case_sens) {
         return str.includes(word);
+    } else {
+        return str.toLowerCase().includes(word.toLowerCase());
     }
 }
 
+// avoids problem with <details open>
 function isSpoilerAlready(comment) {
-    // avoids problem with <details open>
     return comment.innerHTML.startsWith("<details");
 }
 
-setInterval(check_page, 1000);
+function checkState() {
+    chrome.storage.sync.get({ state: false }, function(data) {
+        console.log(data.state);
+        if (data.state) {
+            checkPage();
+        }
+    });
+}
 
+setInterval(checkState, 1000);
--- popup.js
+++ popup.js
@@ -1,24 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('submit_button');
+    var on_off = document.getElementById('onOffToggle');
 
     link.addEventListener('click', function() {
         submit();
     });
 
+    on_off.addEventListener('click', function() {
+        changeState();
+    });
+
     // data is an obj with key value pairings
-    chrome.storage.sync.get({list:[], case_sens:false, exact:false}, function (data) {
+    chrome.storage.sync.get({ list: [], case_sens: false, exact: false, state: false }, function(data) {
         var text = document.getElementById("filter_box");
         document.getElementById("case_sens").checked = data.case_sens;
         document.getElementById("exact").checked = data.exact;
-
+        document.getElementById("onOffToggle").checked = data.state;
 
         var toShow = [];
-        console.log(data.list);
 
-        for(let i = 0; i < data.list.length; i++) {
+        for (let i = 0; i < data.list.length; i++) {
             toShow.push(data.list[i]);
         }
-            
+
         text.innerText = toShow.join(',');
     });
 });
@@ -26,21 +30,32 @@ document.addEventListener('DOMContentLoaded', function() {
 function submit() {
     var text = document.getElementById("filter_box").value;
     var case_sens = document.getElementById("case_sens").checked;
-    var exact = document.getElementById("exact").checked ;
+    var exact = document.getElementById("exact").checked;
 
-    // Sanitise inputs
     var words = text.split(",");
 
     words = words.map(function(word) {
         return word.trim();
     });
 
-    chrome.storage.sync.set({list:words, case_sens:case_sens, exact:exact}, function() {
-        console.log("filtered words are " + words);
+    //Set filter words
+    chrome.storage.sync.set({ list: words, case_sens: case_sens, exact: exact });
+
+    refreshPage();
+
+}
+
+function refreshPage() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(arrayOfTabs) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(arrayOfTabs[0].id, { code: code });
     });
+}
 
-    console.log("case_sens - " + case_sens + "\n" + "exact - " + exact)
+function changeState() {
+    var button = document.getElementById('onOffToggle').checked;
+    chrome.storage.sync.set({ state: button });
 
+    refreshPage();
 
-    
 }
